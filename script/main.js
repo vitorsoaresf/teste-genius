1ea5d0f3ba5e66c37f4390821cea2ad9a2c2bbc4
Vitor Soares <vitorsoaresf@gmail.com>
2024-03-25 18:24:58 -0300

    fea: implements rectangle to onclick event

--- script/main.js
+++ script/main.js
@@ -1,11 +1,12 @@
 const NUMBER_MAX_COLORS = 4;
 
-const colorArray = ["red", "green", "red", "blue"];
+const colorArray = ["red", "green", "yellow", "blue"];
 
 const selectRandomColor = () => {
   return colorArray[Math.floor(Math.random() * NUMBER_MAX_COLORS)];
 };
 
-for (let i = 0; i < NUMBER_MAX_COLORS; i++) {
-  console.log(selectRandomColor());
-}
+const sequenceColors = [];
+sequenceColors.push(selectRandomColor());
+
+console.log(sequenceColors);
