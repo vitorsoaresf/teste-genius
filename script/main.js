89316ebd12c4280bdc2e73c53a05a38f5ea97871
Vitor Soares <vitorsoaresf@gmail.com>
2024-03-25 19:26:12 -0300

    feat: implements logic game

--- script/main.js
+++ script/main.js
@@ -1,14 +1,39 @@
 const NUMBER_MAX_COLORS = 4;
-
 const colorArray = ["red", "green", "yellow", "blue"];
+let sequenceColors = null;
+let countCompareColors = null;
 
 const selectRandomColor = () => {
   return colorArray[Math.floor(Math.random() * NUMBER_MAX_COLORS)];
 };
 
-const sequenceColors = [];
-sequenceColors.push(selectRandomColor());
-
-const verifyWhatColorPressed = (event) => {
-  console.log(event);
+const addColorInTheList = () => {
+  sequenceColors.push(selectRandomColor());
+  console.log(sequenceColors);
 };
+
+const resetGame = () => {
+  countCompareColors = 0;
+  sequenceColors = [];
+  addColorInTheList();
+};
+
+const verifyIfLastElementIsActive = () => {
+  return sequenceColors.length === countCompareColors;
+};
+
+const compareColorPressed = (colorSelected) => {
+  if (sequenceColors[countCompareColors] === colorSelected) {
+    countCompareColors++;
+
+    if (verifyIfLastElementIsActive()) {
+      countCompareColors = 0;
+      addColorInTheList();
+    }
+  } else {
+    resetGame();
+    alert("vc perdeu");
+  }
+};
+
+resetGame();
