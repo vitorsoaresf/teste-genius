b135a789fc0422dc72f3497ac0aa7fd282b73cb8
Vitor Soares <vitorsoaresf@gmail.com>
2024-03-25 17:50:38 -0300

    feat: implements random color from color array

--- script/main.js
+++ script/main.js
@@ -1,1 +1,11 @@
+const NUMBER_MAX_COLORS = 4;
+
 const colorArray = ["red", "green", "red", "blue"];
+
+const selectRandomColor = () => {
+  return colorArray[Math.floor(Math.random() * NUMBER_MAX_COLORS)];
+};
+
+for (let i = 0; i < NUMBER_MAX_COLORS; i++) {
+  console.log(selectRandomColor());
+}
